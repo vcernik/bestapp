9bebb032fd704b84c029f298c7cc3aafc22da42e
Václav Černík <vaclav@cernik.name>
2026-07-24 19:16:51 +0200

    update

--- app/Presentation/Admin/Home/HomePresenter.php
+++ app/Presentation/Admin/Home/HomePresenter.php
@@ -3,18 +3,10 @@
 namespace App\Presentation\Admin\Home;
 
 use App\AdminCore\Presentation\Accessory\BasePrivatePresenter;
-use App\AdminCore\Presentation\Accessory\DatagridFactory;
-use App\Model\Orm\Article\ArticleRepository;
-use Contributte\Datagrid\Datagrid;
 
 final class HomePresenter extends BasePrivatePresenter
 {
-	public function __construct(
-		private readonly ArticleRepository $articleRepository,
-		private readonly DatagridFactory $datagridFactory,
-	)
-	{
-	}
+	
 
 	protected function startup(): void
 	{
--- app/Presentation/Admin/Test2/Test2Presenter.php
+++ app/Presentation/Admin/Test2/Test2Presenter.php
@@ -3,19 +3,9 @@
 namespace App\Presentation\Admin\Test2;
 
 use App\AdminCore\Presentation\Accessory\BasePrivatePresenter;
-use App\AdminCore\Presentation\Accessory\DatagridFactory;
-use App\Model\Orm\Article\ArticleRepository;
-use Contributte\Datagrid\Datagrid;
 
 final class Test2Presenter extends BasePrivatePresenter
-{
-	public function __construct(
-		private readonly ArticleRepository $articleRepository,
-		private readonly DatagridFactory $datagridFactory,
-	)
-	{
-	}
-
+{	
 	protected function startup(): void
 	{
 		parent::startup();
